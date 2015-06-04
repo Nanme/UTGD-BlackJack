0f6000e51326c1d3ef3860c2a20d84a931fcc9fc
Karitori <real90@hotmail.fr>
2015-06-04 17:41:12 +0200

    Working Card Class

---
+++ Card.java
@@ -0,0 +1,38 @@
+public class Card {
+	private int value;
+	private char color;
+
+	Card(){
+		value = 0;
+		color = 0;
+	}
+
+	public int getValue(){
+		return this.value;
+	}
+	public char getColor(){
+		return this.color;
+	}
+	public String toString(){
+		String buffer;
+
+		switch (this.value){
+			case 1:
+				buffer = "A "+this.color;
+				break;
+			case 11:
+				buffer = "J "+this.color;
+				break;
+			case 12:
+				buffer = "Q "+this.color;
+				break;
+			case 13:
+				buffer = "K "+this.color;
+				break;
+			default:
+				buffer = value+" "+this.color;
+				break;
+		}
+		return buffer;
+	}
+}
